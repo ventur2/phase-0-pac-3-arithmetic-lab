e4abaa34ad931b858c390d12ee49b3354c1f1e7c
rachell <dazzlemevanity@gmail.com>
2022-04-12 00:30:14 -0400

    commit tree 3fc90866246c606fc400bdbc80c65dea6ba7a52b

---
+++ index.js
@@ -0,0 +1,27 @@
+function add() {
+    // we'll fill this in shortly
+  }
+
+  function subtract () {
+      
+  }
+  function multiply () {
+
+  }
+  function divide () {
+
+  }
+
+  function add() {
+    return "something";
+  }
+  
+
+  function add(a, b) {
+    return a + b;
+  }
+ 
+  function subtract(a,b) {
+      return b-a;
+  }
+    
